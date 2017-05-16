c7ed558b6b3a591b3e3be09ef80c9f29d8019a7d
Woojgh <jamessalamonsen@gmail.com>
2017-05-16 14:26:08 -0700

    added in the lucas func

--- series.py
+++ series.py
@@ -1,5 +1,4 @@
-"""Constring."""
-
+"""This is a function that finds the nth value in the fibonacci numbers."""
 
 def fibonacci(n):
     a = 0
@@ -15,3 +14,16 @@ def fibonacci(n):
             b = 1
     return result
 
+"""This is a function that will give you the nth value in the Lucas numbers."""
+
+def lucas(n):
+    a = 2
+    b = 1
+    if n < 2:
+        return n
+    while n > 0:
+        result = a + b
+        a = b
+        b = result
+        n -= 1
+    return result
